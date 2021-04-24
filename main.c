b932f13eaf2e68cbcf2d34a6a03aa3b85e94f713
Elias Pereira Gomes Junior <eliasepgj@gmail.com>
2021-04-24 09:28:22 -0300

    sinais bloqueados no tratamento de sigusr1 e sigusr2

--- main.c
+++ main.c
@@ -6,16 +6,28 @@ int main()
 {   
     int pid;
     tShell shell;
+    //Cria mascara de sinais bloqueados
+    sigset_t block;
+    //Inicializa mascara
+    sigemptyset(&block);
+    //Define quais sinais devem ser bloqueados
+    sigaddset(&block, SIGINT);
+    sigaddset(&block, SIGQUIT);
+    sigaddset(&block, SIGTSTP);
 
-
+    
     struct sigaction susr1 = {0};
     susr1.sa_handler = &handle_sigusr1;
     susr1.sa_flags = SA_RESTART;
+    susr1.sa_mask = block;
+
 
     struct sigaction susr2 = {0};
     susr2.sa_handler = &handle_sigusr2;
     susr2.sa_flags = SA_RESTART;
-    
+    susr2.sa_mask = block;
+
+
     sigaction(SIGUSR1, &susr1, NULL);
     sigaction(SIGUSR2, &susr2, NULL);
 
